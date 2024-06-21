fc718122f51fce3ad6fc294c27d4c7ce91954991
Yuvraj <devyuvraj883@gmail.com>
2024-06-22 01:03:19 +0530

    Created why choose us section

---
+++ components/ChooseUs.jsx
@@ -0,0 +1,17 @@
+const ChooseUs = ()=>{
+  return(
+    <>
+    <div className="grid grid-cols-3 gap-6 bg-white m-0">
+
+      <section className="text-red-700 col-span-1 flex-col items-center justify-center shadow-lg ">
+        <img src="/choose1.webp" alt="Flexibility"/>
+        <h1>Flexibility</h1>
+        <p>Security recognition and new opportunities for the candidates.</p>
+      </section>
+
+    </div>
+
+    </>
+  )
+}
+export default ChooseUs;
