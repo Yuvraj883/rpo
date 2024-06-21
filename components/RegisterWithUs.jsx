87b73e3c8b4ad81bb2a2606639e0c23447b8524a
Yuvraj <devyuvraj883@gmail.com>
2024-06-22 01:56:16 +0530

    Created RegisterWithUs section

--- components/RegisterWithUs.jsx
+++ components/RegisterWithUs.jsx
@@ -1,11 +1,19 @@
 const RegisterWithUs = ()=>{
   return(
     <>
-        <div className="flex bg-white py-6 text-red-700">
-          <img src='/register.jpg' alt='Register with us'/>
+        <div className="md:flex flex-col md:px-12 gap-12 items-center justify-center bg-white py-6 text-red-700">
+          <img src='/register.jpg'
+          className="h-80 w-auto rounded-md"
+          alt='Register with us'/>
           <section >
-            <h1>Register With Us!!</h1>
-            <p>Struggling with your dream job search? We could help :{')'}</p>
+            <h1 className="font-bold text-3xl">Register With Us!!</h1>
+            <p className="font-semibold text-lg mb-2">Struggling with your dream job search? We could help :{')'} <br/>
+
+            </p>
+            <ul>
+              <li>Click the button below and get started 🎉🎉</li>
+            </ul>
+            <button className="text-white hover:font-semibold mt-8 bg-red-500 px-8 py-3 rounded-md">Get Started </button>
           </section>
         </div>
     </>
