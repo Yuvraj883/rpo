309a963717eb612f52610652d9113b4f41d719f0
Yuvraj <devyuvraj883@gmail.com>
2024-06-22 01:38:49 +0530

    Created Register with us section

---
+++ components/RegisterWithUs.jsx
@@ -0,0 +1,14 @@
+const RegisterWithUs = ()=>{
+  return(
+    <>
+        <div className="flex bg-white py-6 text-red-700">
+          <img src='/register.jpg' alt='Register with us'/>
+          <section >
+            <h1>Register With Us!!</h1>
+            <p>Struggling with your dream job search? We could help :{')'}</p>
+          </section>
+        </div>
+    </>
+  )
+}
+export default RegisterWithUs;
