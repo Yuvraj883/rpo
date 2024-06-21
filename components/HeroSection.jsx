4109340bdc04fe3ffa296dbeb50d95c1588810e4
Yuvraj <devyuvraj883@gmail.com>
2024-06-22 02:00:00 +0530

    Added Get Started Button

--- components/HeroSection.jsx
+++ components/HeroSection.jsx
@@ -6,6 +6,8 @@ const HeroSection = () => {
         <div className="relative bg-transparent top-48 w-[80%] md:w-[50%] m-auto text-red-700">
           <h1 className=" text-4xl font-bold z-10 ">The best RPO in the world</h1>
           <p className="font-semibold p-2 mt-2 text-lg">ABC Consultants is the pioneer of organised recruitment services in India hiring leaders for the leading organizations. Being one of the top recruitment firms, …  Source: ABC Consultants</p>
+          <button className="mt-8 bg-red-500 px-8 py-3 rounded-md text-white hover:font-semibold">Get Started</button>
+
         </div>
       </div>
     </>
