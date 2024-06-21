483419818256a4d6abc08dc0b1812c4b5e5bb5c3
Yuvraj <devyuvraj883@gmail.com>
2024-06-22 02:00:15 +0530

    Added responsiveness

--- components/RegisterWithUs.jsx
+++ components/RegisterWithUs.jsx
@@ -1,22 +1,25 @@
-const RegisterWithUs = ()=>{
-  return(
+const RegisterWithUs = () => {
+  return (
     <>
-        <div className="md:flex flex-col md:px-12 gap-12 items-center justify-center bg-white py-6 text-red-700">
-          <img src='/register.jpg'
+      <div className="flex flex-col items-center justify-center bg-white py-6 text-red-700 md:flex-row md:gap-12 md:px-12">
+        <img
+          src='/register.jpg'
           className="h-80 w-auto rounded-md"
-          alt='Register with us'/>
-          <section >
-            <h1 className="font-bold text-3xl">Register With Us!!</h1>
-            <p className="font-semibold text-lg mb-2">Struggling with your dream job search? We could help :{')'} <br/>
-
-            </p>
-            <ul>
-              <li>Click the button below and get started 🎉🎉</li>
-            </ul>
-            <button className="text-white hover:font-semibold mt-8 bg-red-500 px-8 py-3 rounded-md">Get Started </button>
-          </section>
-        </div>
+          alt='Register with us'
+        />
+        <section className="text-center md:text-left">
+          <h1 className="font-bold text-3xl">Register With Us!!</h1>
+          <p className="font-semibold text-lg mb-2">
+            Struggling with your dream job search? We could help :) <br/>
+          </p>
+          <ul>
+            <li>Click the button below and get started 🎉🎉</li>
+          </ul>
+          <button className="mt-8 bg-red-500 px-8 py-3 rounded-md text-white hover:font-semibold">Get Started</button>
+        </section>
+      </div>
     </>
-  )
-}
+  );
+};
+
 export default RegisterWithUs;
