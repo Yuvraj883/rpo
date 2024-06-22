3f6f9eb6a5a2b70049c17552f82410fc082a00da
Yuvraj <devyuvraj883@gmail.com>
2024-06-22 12:39:29 +0530

    Corrected error

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,7 +3,7 @@ const Footer = ()=>{
     <>
     <div className="bg-red-500 font-semibold py-2">
         <p className="text-center">We would be glad to help you with your job search.</p>
-        <p className="text-center">For more details e-mail us at jangidharding@abc.in or call at 999999999.</p>
+        <p className="text-center">For more details e-mail us at jangidhardik@abc.in or call at 999999999.</p>
         <p className="text-center">All rights reserved. Copyright@2024</p>
 
     </div>
